f3c68fc1a6836b95678bd5e5448aef18184108b2
lefcott <lefcott@hotmail.com>
2023-02-01 19:26:38 -0300

    fullscreen background video, thanks chatgpt

--- src/components/BackgroundVideo/index.tsx
+++ src/components/BackgroundVideo/index.tsx
@@ -1,7 +1,7 @@
 export default function BackgroundVideo() {
   return (
     <video
-      className="fixed right-0 bottom-0 min-w-[120vw] min-h-[100vh] opacity-30"
+      className="fixed top-0 left-0 w-[100vw] h-[100vh] opacity-30 object-cover"
       autoPlay
       muted
       loop

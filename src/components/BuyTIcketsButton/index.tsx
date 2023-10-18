c20968b5bdcd4421573096491a97f94531031d30
lefcott <lefcott@hotmail.com>
2023-10-18 01:22:53 -0300

    don't show element id in the path

--- src/components/BuyTIcketsButton/index.tsx
+++ src/components/BuyTIcketsButton/index.tsx
@@ -1,8 +1,12 @@
 import Button from "../Button";
 
 export default function BuyTicketsButton() {
+  const handleClick = () => {
+    history.replaceState({}, document.title, window.location.pathname);
+  };
+
   return (
-    <Button isLink href="#comprar-entradas">
+    <Button isLink href="#comprar-entradas" onClick={handleClick}>
       Comprar entradas
     </Button>
   );

48d91996e88df58665495eb66f056dc0884364df
lefcott <lefcott@hotmail.com>
2023-10-18 07:37:11 -0300

    add padding to date

--- src/components/ShowInfo/index.tsx
+++ src/components/ShowInfo/index.tsx
@@ -18,7 +18,7 @@ export default function ShowInfo() {
     <>
       <div className="flex items-center space-x-4 mb-4">
         <FontAwesomeIcon icon={faCalendarDay} />
-        <h1 className="text-2xl">{show.date}</h1>
+        <h1 className="text-2xl pl-1">{show.date}</h1>
       </div>
       <div className="flex items-center space-x-4">
         <FontAwesomeIcon icon={faLocationDot} />

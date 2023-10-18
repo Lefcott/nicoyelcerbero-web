f2a82787603b8f7ccfc35dee20bc8915f3910018
lefcott <lefcott@hotmail.com>
2023-10-18 07:50:53 -0300

    adjust info font size

--- src/components/ShowInfo/index.tsx
+++ src/components/ShowInfo/index.tsx
@@ -22,7 +22,7 @@ export default function ShowInfo() {
       </div>
       <div className="flex items-center space-x-4">
         <FontAwesomeIcon icon={faLocationDot} />
-        <span className="text-base pl-2">
+        <span className="text-sm pl-2">
           {show.locationName} (
           <Link href={show.addressUrl} target="_blank">
             <span className="text-teal-400">{show.address}</span>
@@ -33,13 +33,13 @@ export default function ShowInfo() {
       {show.isFree && (
         <div className="flex items-center space-x-4">
           <FontAwesomeIcon icon={faTicket} />
-          <span className="text-base pl-[2px]">Entrada gratis</span>
+          <span className="text-sm pl-[2px]">Entrada gratis</span>
         </div>
       )}
       {!show.isFree && show.presalePrice && (
         <div className="flex items-center space-x-4">
           <FontAwesomeIcon icon={faTicket} />
-          <span className="text-base pl-[2px]">
+          <span className="text-sm pl-[2px]">
             Entrada anticipada: $ {show.presalePrice}
           </span>
         </div>
@@ -47,14 +47,14 @@ export default function ShowInfo() {
       {!show.isFree && show.indoorPrice && (
         <div className="flex items-center space-x-4">
           <FontAwesomeIcon icon={faTicket} />
-          <span className="text-base pl-[2px]">
+          <span className="text-sm pl-[2px]">
             Entrada en puerta: $ {show.indoorPrice}
           </span>
         </div>
       )}
       <div className="flex items-center space-x-4">
         <FontAwesomeIcon icon={faUser} />
-        <span className="text-base pl-[6px]">
+        <span className="text-sm pl-[6px]">
           {show.onlyAdults
             ? "Evento para mayores de 18 años"
             : "Evento apto para todo público"}
@@ -64,7 +64,7 @@ export default function ShowInfo() {
         <FontAwesomeIcon icon={faVideo} />
         <Link
           href="#videos-de-shows-anteriores"
-          className="text-base pl-[2.5px] text-teal-400"
+          className="text-sm pl-[2.5px] text-teal-400"
           onClick={cleanPath}
         >
           Ver videos de nuestros shows anteriores

60f9507ba039e258ddc8d62af312eafda32a4bfa
lefcott <lefcott@hotmail.com>
2023-10-25 05:20:19 -0300

    adapt page

--- src/pages/success/[payerEmail]/[guestsCount].tsx
+++ src/pages/success/[payerEmail]/[guestsCount].tsx
@@ -5,9 +5,10 @@ import { useRouter } from "next/router";
 
 export default function SuccessPage() {
   const { query } = useRouter();
-  const { payerEmail, showDate } = query;
-  const guests = (query.guests && JSON.parse(query.guests as string)) || [];
-  const conditionalS = guests.length > 1 ? "s" : "";
+  const { payerEmail } = query;
+  const guestsCount = +(query.guestsCount as string);
+  const conditionalS = guestsCount > 1 ? "s" : "";
+  const conditionalCount = guestsCount > 1 ? ` ${guestsCount}` : "";
 
   return (
     <>
@@ -16,25 +17,22 @@ export default function SuccessPage() {
       </Head>
       <Layout>
         <h1 className="text-xl max-w-[80%] md:max-w-[50%]">
-          Ya tenés tu{conditionalS} entrada{conditionalS} para el show de
-          Ácidamente del {showDate}!
+          Ya tenés tu{conditionalS}
+          {conditionalCount} entrada{conditionalS} para el show de Ácidamente!
         </h1>
         <p className="pt-4 max-w-[80%] md:max-w-[50%] text-sm">
-          Anotamos a las siguientes personas en la lista de invitados:
-        </p>
-        <ul className="pt-4">
-          {guests.map((guest, i) => (
-            <div
-              key={i}
-              className="bg-gray-500 p-1 rounded-sm m-3 flex justify-center items-center"
-            >
-              {guest.firstName} {guest.lastName}
-            </div>
-          ))}
-        </ul>
-        <p className="pt-4 max-w-[80%] md:max-w-[50%] text-sm">
-          Para acceder al evento, las personas en la lista solo tendrán que
-          decir su nombre y apellido en la entrada.
+          {guestsCount > 1 ? (
+            <>
+              Anotamos el nombre y apellido de las personas en la lista de
+              invitados. Para acceder al evento, los invitados solo tendrán que
+              decir su nombre y apellido en la entrada.
+            </>
+          ) : (
+            <>
+              Ya te anotamos en la lista de invitados. Para acceder al evento,
+              solo tendrás que decir tu nombre y apellido en la entrada.
+            </>
+          )}
         </p>
         <p className="pt-4 max-w-[80%] md:max-w-[50%] text-sm">
           Te enviamos un email a {payerEmail} para que tengas esta información a
